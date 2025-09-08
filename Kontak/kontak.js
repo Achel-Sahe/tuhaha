50ee57e7729ff1300703ff9d92f4a0adeb967a99
Achel-Sahe <achelsahetapy10@gmail.com>
2025-09-08 14:14:04 +0600

    kontak section'

---
+++ Kontak/kontak.js
@@ -0,0 +1,20 @@
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbz3E6G2zE5Kkvp5sg1avAQJ3VahxUEUTmXmjvjIjvQHovvcxtpdVnvw2jxSq_1sCtei/exec'
+  const form = document.forms['Masukan-negeri-tuhaha']
+  const btnKirim = document.querySelector('.btn-kirim')
+  const btnLoading = document.querySelector('.btn-loading')
+  const alert = document.querySelector('.alert')
+
+    form.addEventListener('submit', e => {
+        e.preventDefault()
+        btnLoading.classList.toggle('hidden');
+        btnKirim.classList.toggle('hidden');
+        fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            btnLoading.classList.toggle('hidden');
+            btnKirim.classList.toggle('hidden');
+            alert.classList.toggle('hidden');
+            form.reset();
+            console.log('Success!', response)
+        })
+      .catch(error => console.error('Error!', error.message))
+  })

0441e3aefc789de71fac42a8243c4a94ca88a58a
Achel-Sahe <achelsahetapy10@gmail.com>
2025-09-13 13:19:52 +0600

    font kontak

--- Kontak/kontak.js
+++ Kontak/kontak.js
@@ -1,11 +1,13 @@
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbz3E6G2zE5Kkvp5sg1avAQJ3VahxUEUTmXmjvjIjvQHovvcxtpdVnvw2jxSq_1sCtei/exec'
-  const form = document.forms['Masukan-negeri-tuhaha']
-  const btnKirim = document.querySelector('.btn-kirim')
-  const btnLoading = document.querySelector('.btn-loading')
-  const alert = document.querySelector('.alert')
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz3E6G2zE5Kkvp5sg1avAQJ3VahxUEUTmXmjvjIjvQHovvcxtpdVnvw2jxSq_1sCtei/exec'
+const form = document.forms['Masukan-negeri-tuhaha']
+const btnKirim = document.querySelector('.btn-kirim')
+const btnLoading = document.querySelector('.btn-loading')
+const alert = document.querySelector('.alert')
+const btnKontak = document.querySelector('#btnKontak');
+btnKontak.addEventListener("click", function () {alert.classList.toggle('hidden');})
 
-    form.addEventListener('submit', e => {
-        e.preventDefault()
+form.addEventListener('submit', e => {
+  e.preventDefault()
         btnLoading.classList.toggle('hidden');
         btnKirim.classList.toggle('hidden');
         fetch(scriptURL, { method: 'POST', body: new FormData(form)})
@@ -18,7 +20,4 @@
         })
       .catch(error => console.error('Error!', error.message))
     })
-  
-const btnKontak = document.querySelector('#btnKontak');
-btnKontak.addEventListener("click", function () {alert.classList.toggle('hidden');})
 
